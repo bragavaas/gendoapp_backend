14185017d7eeab8bd8ef372316308f96581e9a10
bragavaas <bragavaas@gmail.com>
2022-10-11 08:33:46 -0300

    Initial commit for Spring project

---
+++ src/test/java/com/vasco/gendo/GendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasco.gendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
